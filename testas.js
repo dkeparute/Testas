40db9c99cddf165cdb6898ad2f71b76add158529
Dovilė <dovile.keparute@gmail.com>
2021-08-12 18:23:50 +0300

    Testas: ziniu patikrinimas

--- testas.js
+++ testas.js
@@ -1,55 +1,104 @@
 const data = [
     {
         name: 'Jonas',
-        marks: [10, 2, 8, 4, 6] //6
+        marks: [10, 2, 8, 4, 6]
     },
     {
         name: 'Maryte',
-        marks: [3, 7, 9, 5] // 5,75
+        marks: [3, 7, 9, 5]
     },
     {
         name: 'Petras',
-        marks: [7, 7, 7] //7
+        marks: [7, 7, 7]
     },
     {
         name: 'Ona',
-        marks: [10, 9, 8, 7, 6] // 8
+        marks: [10, 9, 8, 7, 6]
     },
 ]
+
 // PASTABA: jei kazkuri reikalavima atitinka daugiau nei vienas mokinys, spausdinti visus
 
-// // Isspausdinti mokinio varda ir pazymi, kuris turi prasciausia pazymi
-console.log("Prasciausias pazimys yra Jono",data[0].marks[1], "is jo visu pazymiu", data[0]);
-// // Isspausdinti mokinio varda ir pazymi, kuris turi geriausia pazymi
-console.log("Geriausias pazimys yra Jono", data[0].marks[0], "is jo visu pazymiu", data[0]);
-console.log("Geriausias pazimys yra Onos", data[3].marks[0], "is jos visu pazymiu", data[3] );
-// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
-console.log("Geriausias pazimiu vidurkis yra:", (data[1].name));
-var sum = 0;
-for (var i = 0; i < data[1].marks.length; i++) {
+// 1. Isspausdinti mokinio varda ir pazymi, kuris turi prasciausia pazymi
+for (let i = 0; i < data.length; i++) {
+    let prasciausiasPazymys = false;
+    for (let j = 0; j < data[i].marks.length; j++) {
+        if (data[i].marks[j] < 3) {
+            prasciausiasPazymys = true;
+            console.log("1.Prasciausia pazymi turi", data[i].name);
+        }
+    }
+}
+console.log("------------------------");
+// 2. Isspausdinti mokinio varda ir pazymi, kuris turi geriausia pazymi
+for (let i = 0; i < data.length; i++) {
+    let GeriausiasPazymys = false;
+    for (let j = 0; j < data[i].marks.length; j++) {
+        if (data[i].marks[j] > 9) {
+            GeriausiasPazymys = true;
+            console.log("2.Geriausia pazymi turi", data[i].name);
+        }
+    }
+}
+console.log("----------------------------------------------");
+// 3. Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
+console.log("VISU MOKINIU VIDURKIAI");
+// Jono vidurkis 6
+console.log(data[0].name);
+sum = 0;
+for (i = 0; i < data[0].marks.length; i++) {
+    sum += data[0].marks[i];
+}
+vidurkis = sum / data[0].marks.length;
+console.log(vidurkis);
+// Marytes vidurkis 6
+console.log(data[1].name);
+sum = 0;
+for (i = 0; i < data[1].marks.length; i++) {
     sum += data[1].marks[i];
 }
-let vid = sum / data[1].marks.length;
-console.log(vid);
-// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi geriausia pazymiu vidurki
-console.log("Geriausias pazimiu vidurkis yra:", (data[3].name));
-var suma = 0;
-for (var i = 0; i < data[3].marks.length; i++) {
+vidurkis = sum / data[1].marks.length;
+console.log(vidurkis);
+// Petro vidurkis 7
+console.log(data[2].name);
+sum = 0;
+for (i = 0; i < data[2].marks.length; i++) {
+    sum += data[2].marks[i];
+}
+vidurkis = sum / data[2].marks.length;
+console.log(vidurkis);
+// Onos vidurkis 8
+console.log(data[3].name);
+sum = 0;
+for (i = 0; i < data[3].marks.length; i++) {
+    sum += data[3].marks[i];
+}
+vidurkis = sum / data[3].marks.length;
+console.log(vidurkis);
+console.log("----------------------------------------------");
+console.log("3.Prasciausias pazymys yra pas:", data[0].name, "ir", data[1].name);
+console.log("----------------------------------------------")
+// 4. Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi geriausia pazymiu vidurki
+console.log("4.Geriausias pazymys yra pas:", data[3].name);
+suma = 0;
+for (i = 0; i < data[3].marks.length; i++) {
     suma += data[3].marks[i];
 }
-let vidurkis = suma / data[3].marks.length;
+vidurkis = suma / data[3].marks.length;
 console.log(vidurkis);
-// Isspausdinti mokinio varda, jei jis turi pazymi 8
+console.log("----------------------------------------------");
+// 5. Isspausdinti mokinio varda, jei jis turi pazymi 8
 for (let i = 0; i < data.length; i++) {
     let pazymysAstuoni = false;
     for (let j = 0; j < data[i].marks.length; j++) {
         if (data[i].marks[j] == 8) {
             pazymysAstuoni = true;
-            console.log(data[i].name, "turi pazymi 8")
+            console.log("5.Pazymi 8 turi", data[i].name);
         }
     }
 }
-// // Isspausdinti mokinio varda, jei jis neturi pazymio 7
+console.log("----------------------------------------------");
+// 6. Isspausdinti mokinio varda, jei jis neturi pazymio 7
 for (let i = 0; i < data.length; i++) {
     let pazymysSeptyni = false;
     for (let j = 0; j < data[i].marks.length; j++) {
@@ -58,7 +107,7 @@ for (let i = 0; i < data.length; i++) {
         }
     }
     if (!pazymysSeptyni) {
-        console.log(data[i].name, "neturi pazymio 7");
+        console.log("6.Pazymio 7 neturi", data[i].name);
     }
 }
-
+console.log("----------------------------------------------");
